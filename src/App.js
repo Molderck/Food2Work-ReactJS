5dd4f69eed3b8913d5dcb5ad276340834784951b
Molderck <sebastiansalazaro@hotmail.com>
2018-12-21 23:53:53 -0500

    Project Structure and React Components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { recipes } from "./tempList";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <RecipeList />
+        <RecipeDetails />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
