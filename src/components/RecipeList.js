6181719d6bd050c3e36322c4078177b62d52e8f8
Molderck <sebastiansalazaro@hotmail.com>
2018-12-25 17:04:03 -0500

    Edited Recipe Component to display recipes

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,11 +4,24 @@ import RecipeSearch from "./RecipeSearch";
 
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
       <React.Fragment>
-        <h1>Hello From List</h1>
         <RecipeSearch />
-        <Recipe />
+        <div className="container my-5">
+          {/*Title*/}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">Recipe List</h1>
+            </div>
+          </div>
+          {/*End of Title*/}
+          <div className="row">
+            {recipes.map(recipe => {
+              return <Recipe key={recipe.recipe_id} recipe={recipe} />;
+            })}
+          </div>
+        </div>
       </React.Fragment>
     );
   }
